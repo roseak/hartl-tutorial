b5962c5e9c7592d7c91a3de929d691ab3fb8c223
roseak <rose.kohn@gmail.com>
2015-10-09 11:47:11 -0600

    Added test for signup page.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,5 +4,6 @@ class UsersControllerTest < ActionController::TestCase
   test "should get signup" do
     get :new
     assert_response :success
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
 end
